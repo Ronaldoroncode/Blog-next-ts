1eb526cbefc9dc48868cd63d37b1706c0de10abe
ronaldoroncode <roncodeit@gmail.com>
2024-11-18 11:16:40 -0300

    fixado header no topo, nav em baixo

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,16 +25,25 @@ export default function RootLayout({
     </header>
   );
   const nav = (
-    <nav>
-      <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Pixegami</h3>
-      </div>
+    <nav>      
+      <Link href="/">
+        <h1>Home</h1>
+      </Link>
+      <Link href="/">
+        <h1>Abaut</h1>
+      </Link>
+      <Link href="/">
+        <h1>Contact</h1>
+      </Link>
+      <Link href="/">
+        <h1>Youtube</h1>
+      </Link>
     </nav>
   );
   const aside = (
     <aside>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Pixegami</h3>
+        <h3>ASIDE BOX</h3>
       </div>
     </aside>
   );
